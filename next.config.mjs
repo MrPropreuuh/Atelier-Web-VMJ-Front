14427930ebced68ca33209b8fd1b7c47bd790fcb
MrPropreuuh <vincent.fougere@epsi.fr>
2024-05-15 17:07:35 +0200

    e

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+export default {
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: 'http://localhost:5000/api/:path*'
+            }
+        ];
+    }
+};

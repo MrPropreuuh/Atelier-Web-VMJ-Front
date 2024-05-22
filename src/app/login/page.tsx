5cf0bbd5826b8e0745800b4152b838676077eaf1
Jules Herrero <91459273+jules789@users.noreply.github.com>
2024-05-22 11:07:38 +0200

    Add files via upload
    login page commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,82 @@
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Tabs,
+  TabsContent,
+  TabsList,
+  TabsTrigger,
+} from "@/components/ui/tabs";
+
+export function TabsDemo() {
+  return (
+    <div className="flex items-center justify-center min-h-screen ">
+      <Tabs defaultValue="account" className="w-[500px] border rounded-lg shadow-lg ">
+        <TabsList className="grid w-full grid-cols-2 border-b">
+          <TabsTrigger value="account">Connexion</TabsTrigger>
+          <TabsTrigger value="password">Inscription</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">
+          <Card>
+            <CardHeader>
+              <CardTitle>Connexion</CardTitle>
+              <CardDescription>
+                Connectez-vous ci-dessous :
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="username">Pseudo/email</Label>
+                <Input id="username" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Mot de passe</Label>
+                <Input id="password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Connexion</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+        <TabsContent value="password">
+          <Card>
+            <CardHeader>
+              <CardTitle>Inscription</CardTitle>
+              <CardDescription>
+                Inscrivez-vous ci-dessous :
+              </CardDescription>
+            </CardHeader>
+            <CardContent className="space-y-2">
+              <div className="space-y-1">
+                <Label htmlFor="email">Adresse e-mail</Label>
+                <Input id="email" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="username">Pseudo</Label>
+                <Input id="username" />
+              </div>
+              <div className="space-y-1">
+                <Label htmlFor="password">Mot de passe</Label>
+                <Input id="password" type="password" />
+              </div>
+            </CardContent>
+            <CardFooter>
+              <Button>Inscription</Button>
+            </CardFooter>
+          </Card>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
+}
+
+export default TabsDemo;

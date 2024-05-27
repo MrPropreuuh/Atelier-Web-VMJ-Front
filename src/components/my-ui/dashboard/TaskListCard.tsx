1827c6b61fe2d7d6e47c7ef256f3f2452814d72e
MrPropreuuh <vincent.fougere@epsi.fr>
2024-05-28 01:18:58 +0200

    ajout color select

--- src/components/my-ui/dashboard/TaskListCard.tsx
+++ src/components/my-ui/dashboard/TaskListCard.tsx
@@ -14,15 +14,17 @@ interface Task {
 }
 
 const initialTasks: Task[] = [
-    { priorite: 'Haute', name: 'Liaison entre toutes les pages', assignedTo: 'VMJ', dateToFinish: '11/04', status: 'A faire' },
-    { priorite: 'Haute', name: "Ajout des œuvres d'art faite maison par l'artiste Maxime", assignedTo: 'Maxime', dateToFinish: '11/04', status: 'En cours' },
-    { priorite: 'Haute', name: 'Ajout backend', assignedTo: 'VMJ', dateToFinish: '11/04', status: 'En cours' },
-    { priorite: 'Moyen', name: 'Ajout dashboard', assignedTo: 'Vincent', dateToFinish: '11/04', status: 'En cours' },
-    { priorite: 'Basse', name: 'Ajout de page commande fictif', assignedTo: 'Vincent', dateToFinish: '11/04', status: 'Terminé' },
-    { priorite: 'Basse', name: 'Ajout page contact', assignedTo: 'Jules', dateToFinish: '11/04', status: 'Terminé' },
-    { priorite: 'Basse', name: 'Ajout navbar', assignedTo: 'Maxime', dateToFinish: '11/04', status: 'Terminé' },
-    { priorite: 'Moyen', name: 'Ajout page login/register', assignedTo: 'Jules', dateToFinish: '11/04', status: 'En cours' },
-    { priorite: 'Haute', name: 'Ajout page accueil', assignedTo: 'Maxime', dateToFinish: '11/04', status: 'En cours' },
+    { priorite: 'Basse', name: 'Peaufinage du site', assignedTo: 'VMJ', dateToFinish: '28/04', status: 'En cours' },
+    { priorite: 'Haute', name: 'Liaison entre toutes les pages', assignedTo: 'VMJ', dateToFinish: '28/04', status: 'A faire' },
+    { priorite: 'Haute', name: "Ajout des œuvres d'art faite maison par l'artiste Maxime", assignedTo: 'Maxime', dateToFinish: '26/04', status: 'Terminé' },
+    { priorite: 'Haute', name: 'Ajout backend', assignedTo: 'VMJ', dateToFinish: '28/04', status: 'En cours' },
+    { priorite: 'Moyen', name: 'Ajout dashboard', assignedTo: 'Vincent', dateToFinish: '22/04', status: 'En cours' },
+    { priorite: 'Basse', name: 'Ajout de page commande fictif', assignedTo: 'Vincent', dateToFinish: '20/04', status: 'Terminé' },
+    { priorite: 'Basse', name: 'Ajout page contact', assignedTo: 'Jules', dateToFinish: '23/04', status: 'Terminé' },
+    { priorite: 'Basse', name: 'Ajout navbar', assignedTo: 'Maxime', dateToFinish: '24/04', status: 'Terminé' },
+    { priorite: 'Moyen', name: 'Ajout page login/register', assignedTo: 'Jules', dateToFinish: '23/04', status: 'En cours' },
+    { priorite: 'Basse', name: 'Mise en place syteme connexion/inscriptions', assignedTo: 'Jules', dateToFinish: '26/04', status: 'Terminé' },
+    { priorite: 'Haute', name: 'Ajout page accueil', assignedTo: 'Maxime', dateToFinish: '24/04', status: 'En cours' },
 ];
 
 const TaskListCard: React.FC = () => {
